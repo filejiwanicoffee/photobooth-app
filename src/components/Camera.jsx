c9993218926bfb3a8460259a16e4a306101d552b
filejiwanicoffee <filejiwanicoffee@gmail.com>
2026-04-29 23:49:24 +0700

    Update Camera.jsx

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,1 +1,23 @@
+import Webcam from "react-webcam";
+import { useRef } from "react";
 
+export default function Camera({ onCapture }) {
+  const webcamRef = useRef(null);
+
+  const capture = () => {
+    const image = webcamRef.current.getScreenshot();
+    onCapture(image);
+  };
+
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        screenshotFormat="image/png"
+        videoConstraints={{ facingMode: "user" }}
+        style={{ width: "100%", borderRadius: "15px" }}
+      />
+      <button className="capture-hidden" onClick={capture} style={{ display: "none" }} />
+    </div>
+  );
+}
