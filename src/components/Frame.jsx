8cf03c02ce7904b0565861498217f067e14843cc
filejiwanicoffee <filejiwanicoffee@gmail.com>
2026-04-29 23:49:57 +0700

    Update Frame.jsx

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -1,1 +1,16 @@
-
+export default function Frame({ image }) {
+  return (
+    <div style={{ position: "relative" }}>
+      <img src={image} style={{ width: "100%" }} />
+      <img
+        src="/frames/frame1.png"
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+        }}
+      />
+    </div>
+  );
+}
