6ce8221337e1a31b14b4e8f8d15743a0a5f95fba
filejiwanicoffee <filejiwanicoffee@gmail.com>
2026-04-29 23:50:14 +0700

    Update Result.jsx

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,1 +1,36 @@
+import html2canvas from "html2canvas";
+import { useRef } from "react";
 
+export default function Result({ images }) {
+  const ref = useRef();
+
+  const download = async () => {
+    const canvas = await html2canvas(ref.current);
+    const link = document.createElement("a");
+    link.download = "photostrip.png";
+    link.href = canvas.toDataURL();
+    link.click();
+  };
+
+  return (
+    <div>
+      <div
+        ref={ref}
+        style={{
+          width: "200px",
+          margin: "auto",
+          background: "white",
+          padding: "10px"
+        }}
+      >
+        {images.map((img, i) => (
+          <img key={i} src={img} style={{ width: "100%", marginBottom: "8px" }} />
+        ))}
+      </div>
+
+      <button className="button" onClick={download}>
+        Download
+      </button>
+    </div>
+  );
+}
